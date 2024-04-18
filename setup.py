addaa402906ee51f840347c333fcd9efd5ba593d
Jakub Andrysek <jakub.andrysek03@gmail.com>
2024-04-19 01:46:21 +0200

    Update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements():
 # https://pypi.org/project/mkdoxy/
 setup(
     name="mkdoxy",
-    version="1.2.0",
+    version="1.2.1",
     description="MkDoxy → MkDocs + Doxygen = easy documentation generator with code snippets",
     long_description=readme(),
     long_description_content_type="text/markdown",
