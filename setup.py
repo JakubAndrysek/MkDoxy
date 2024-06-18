1702d0f13cb7cac74ba295e12f299934aa9c292d
Jakub Andrysek <jakub.andrysek03@gmail.com>
2024-06-18 10:17:19 +0200

    chore: improve setup.py dependencies

--- setup.py
+++ setup.py
@@ -11,14 +11,28 @@ def requirements():
         return f.read().splitlines()
 
 
+def import_requirements():
+    """Imports requirements from requirements.txt file."""
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def import_dev_requirements():
+    """Imports requirements from devdeps.txt file."""
+    with open("devdeps.txt") as f:
+        return f.read().splitlines()
+
+
 # https://pypi.org/project/mkdoxy/
 setup(
     name="mkdoxy",
-    version="1.2.4",
+    version="1.2.5",
     description="MkDoxy → MkDocs + Doxygen = easy documentation generator with code snippets",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    keywords="mkdoxy, python, open-source, documentation, mkdocs, doxygen, multilanguage, code-snippets, code, snippets, documentation-generator",  # noqa: E501
+    keywords="mkdoxy, python, open-source, documentation, mkdocs, doxygen, "
+    "multilanguage, code-snippets, code, snippets, documentation-generator",
+    # noqa: E501
     url="https://github.com/JakubAndrysek/MkDoxy",
     author="Jakub Andrýsek",
     author_email="email@kubaandrysek.cz",
@@ -30,23 +44,16 @@ setup(
         "Tracker": "https://github.com/JakubAndrysek/MkDoxy/issues",
         "Funding": "https://github.com/sponsors/jakubandrysek",
     },
-    install_requires=["mkdocs"],
+    install_requires=import_requirements(),
     extras_require={
-        "dev": [
-            "mkdocs-material~=9.5.18",
-            "Jinja2~=3.1.3",
-            "mkdocs-open-in-new-tab~=1.0.2",
-            "pathlib~=1.0.1",
-            "isort~=5.12.0",
-            "pytest~=6.2.5",
-            "pre-commit~=3.7.0",
-        ],
+        "dev": import_dev_requirements(),
     },
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
